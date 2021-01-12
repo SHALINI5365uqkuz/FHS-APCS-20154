f63ba4b1b25cdbbf8f38df44cd33347fc7faec15
SHALINI5365uqkuz <nbbkkyd87@163.com>
2021-01-13 00:25:30 +0800

    add helper methods to template code

--- src/template/EmployeeRecords.java
+++ src/template/EmployeeRecords.java
@@ -1,15 +1,24 @@
 package template;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import template.Employee;
 
 public class EmployeeRecords {
 	private ArrayList<Employee> employees;
 	
-	public EmployeeRecords() {
+	/***
+	 * constructs a new records object representing a single day of card swipes.  it is pre-populated
+	 * with a number of consecutive employee ids gien by numids.
+	 * 
+	 * @param numids the number of consecutive employee ids to pre-populate the object with
+	 */
+	public EmployeeRecords(int numids) {
 		employees = new ArrayList<Employee>();
-		createEmployees();
-		
-		// TODO:  Add whatever you want here (then delete this comment)
+		createEmployees(numids);
 	}
 	
 	/***
@@ -17,9 +26,11 @@ public class EmployeeRecords {
 	 * These are the ids that will be in the actual swipes in the data file.
 	 * 
 	 * You may modify this method and/or the employee constructor if you wish.
+	 * 
+	 * @param numids the number of consecutive ids to populate your employee list with
 	 */
-	private void createEmployees() {
-		for (int id = 0; id < 300; id++) {
+	private void createEmployees(int numids) {
+		for (int id = 0; id < numids; id++) {
 			employees.add( new Employee(id) );
 		}
 	}
@@ -90,7 +101,7 @@ public class EmployeeRecords {
    * @param time the time (seconds after midnight) you want an employee list for
    * @return the list of employees in the building at that time
    */
-  public ArrayList<Employee> getEmployeesInBuildingAt(int time) {
+  public ArrayList<Employee> getEmployeesInBuildingAt(long time) {
   	return null;
   }
   
@@ -103,4 +114,56 @@ public class EmployeeRecords {
   public int[] timeIntervalWithMostEmployees() {
   	return null;
   }
+  
+  public void displayAllEmployees() {
+  	for (Employee e:this.employees) {
+  		System.out.println(e);
+  	}
+  }
+  
+  public static double toHours(double seconds) {
+  	return seconds/60.0/60.0;
+  }
+  
+  public static double toSeconds(double hours) {
+  	return hours*60.0*60.0;
+  }
+  
+  /***
+   * Load swipe data from a file.
+   * 
+   * pre-condition: File first line is the # of records, second line is column names
+   *   and remaining lines are comma-separated values.  First column is employee id, second is
+   *   time (in seconds since midnight).  We assume data is in increasing time-sequential order.
+   *   
+   * @param filepath
+   */
+	public void loadDataFromFile(String filepath) {
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new FileReader(filepath));
+			scanner.useDelimiter("\n");
+
+			String line;
+			int numRecords = scanner.nextInt(); 	// first line gives the number of records
+			scanner.next();
+			
+			while (scanner.hasNext()) {
+				line = scanner.next();
+				String[] args = line.split(",");
+
+				try {
+					int id = Integer.parseInt(args[0].trim());
+					long time = Long.parseLong(args[1].trim());
+					
+					this.registerSwipe(id, time);
+					
+				} catch (Exception e) {
+					System.out.println("something went wrong:" + e.getMessage());
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found " + filepath);
+		}
+	}
 }
--- src/template/Simulator.java
+++ src/template/Simulator.java
@@ -13,54 +13,11 @@ import java.util.Scanner;
  *
  */
 public class Simulator {
-	private static final int ID = 0;
-	private static final int TIME = 1;
-
 	public static void main(String[] args) {
-		EmployeeRecords records = new EmployeeRecords();
+		EmployeeRecords records = new EmployeeRecords(300);
 
-		long[][] swipeData = loadDataFromFile("swipeData.csv");
+		records.loadDataFromFile("swipeData.csv");
 		
-		for (int i = 0; i < swipeData.length; i++) {
-			records.registerSwipe((int)swipeData[i][ID], swipeData[i][TIME]);
-		}
+		records.displayAllEmployees();
 	}
-
-	private static long[][] loadDataFromFile(String filepath) {
-		Scanner scanner;
-		try {
-			scanner = new Scanner(new FileReader(filepath));
-			scanner.useDelimiter("\n");
-
-			String line;
-			int numRecords = scanner.nextInt(); 	// first line gives the number of records
-			long[][] records = new long[numRecords][2];
-			
-			scanner.next();												// skip the next line
-			
-			int i = 0;
-			while (scanner.hasNext()) {
-				line = scanner.next();
-				String[] args = line.split(",");
-
-				try {
-					int id = Integer.parseInt(args[ID].trim());
-					long time = Long.parseLong(args[TIME].trim());
-					
-					records[i][ID] = id;
-					records[i][TIME] = time;
-					i++;
-				} catch (Exception e) {
-					System.out.println("something went wrong:" + e.getMessage());
-				}
-			}
-			
-			return records;
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found " + filepath);
-		}
-		
-		return null;
-	}
-
 }
